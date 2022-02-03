6ba4a6db7d23c095316dc0aa1539726b99011f93
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-02-03 15:18:36 +0530

    Create .Divisors and Reciprocals.cpp

---
+++ Math/.Divisors and Reciprocals.cpp
@@ -0,0 +1,44 @@
+//Ref: https://www.codechef.com/START24B/problems/DIVREC
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    
+    int t;
+    cin >> t;
+    
+    while(t--){
+        
+        ll x,a,b;
+        cin >> x >> a >> b;
+        
+        if((x*b)%a != 0){
+            cout << -1 << endl;
+            continue;
+        }
+        
+        ll n = ((ll)x*b)/(ll)a;
+        ll sum = 0;
+        
+        if(n > x){
+            cout << -1 << endl;
+            continue;
+        }
+        for(ll i = 1; i <= sqrt(n); i++){
+            if(n % i == 0){
+                sum += i;
+                sum += n/i;
+                if(n/i == i) sum -= i;
+            }
+        }
+        if(sum == x){
+            cout << n << endl;
+        }
+        else{
+            cout << -1 << endl;
+        }
+    }
+	return 0;
+}
